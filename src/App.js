4e07cecc25c58d48218e95f5e101a164366604fa
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-11-11 17:43:45 -0300

    Configure dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./store";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import AnimatedRoutes from "hocs/routes/Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>Murkiva | Dashboard</title>
+        <meta
+          name="description"
+          content="Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones."
+        />
+        <meta
+          name="keywords"
+          content="agencia de software, agencia de marketing, creacion de pagina web"
+        />
+        <meta name="robots" content="all" />
+        <link rel="canonical" href="https://www.murkiva.com/" />
+        <meta name="author" content="Murkiva" />
+        <meta name="publisher" content="Murkiva" />
+
+        {/* Social Media Tags */}
+        <meta property="og:title" content="Murkiva | Software Agency" />
+        <meta
+          property="og:description"
+          content="Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones."
+        />
+        <meta property="og:url" content="https://www.murkiva.com/" />
+        <meta
+          property="og:image"
+          content="https://bafybeicwrhxloesdlojn3bxyjqnxgsagtd4sl53a7t4cn4vfe2abmybzua.ipfs.w3s.link/lightbnuilbg.jpg"
+        />
+
+        <meta name="twitter:title" content="Murkiva | Software Agency" />
+        <meta
+          name="twitter:description"
+          content="Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones."
+        />
+        <meta
+          name="twitter:image"
+          content="https://bafybeicwrhxloesdlojn3bxyjqnxgsagtd4sl53a7t4cn4vfe2abmybzua.ipfs.w3s.link/lightbnuilbg.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+      <Provider store={store}>
+        <Router>
+          <AnimatedRoutes />
+        </Router>
+      </Provider>
+    </HelmetProvider>
   );
 }
 
